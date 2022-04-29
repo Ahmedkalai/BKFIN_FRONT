105f79c8b396276b136ec3b04c91539ca683044e
Ahmedkalai <49247572+Ahmedkalai@users.noreply.github.com>
2022-04-29 22:12:42 +0100

    Update admin-credit.component.ts

--- src/app/components/pages/admin-credit/admin-credit.component.ts
+++ src/app/components/pages/admin-credit/admin-credit.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import {Credit} from '/src/app/models/Credit';
 
 
 @Component({
@@ -10,14 +9,6 @@ import {Credit} from '/src/app/models/Credit';
   styleUrls: ['./admin-credit.component.css']
 })
 export class AdminCreditComponent implements OnInit {
-  closeResult = '';
-  page: number = 1;
-  count: number = 0;
-  tableSize: number = 6;
-  tableSizes: any = [3, 6, 9, 12];
-
-  Guarantors:Credit[];
-  filterTerm!: string;
 
   constructor() { }
 
